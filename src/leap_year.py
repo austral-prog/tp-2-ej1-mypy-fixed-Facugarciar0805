1572aa436ac51f172e8a14435b9d505a2c03b861
Facundo Garcia Rosselli <facugarciar@gmail.com>
2024-08-25 22:02:13 -0300

    Entregado

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,3 +1,18 @@
-# Replace the "ANSWER HERE" for your answer
 def is_leap_year():
-    return "ANSWER HERE" # Remove this line and implement
+
+    year : int= int(input("Ingrese un año: "))  # type: ignore
+    if year % 4==0:
+        if year % 100==0:
+            if year % 400==0:
+                print(f"El año {year} es bisiesto")
+                return True
+            else:
+                print(f"El año {year} no es bisiesto")
+                return False
+        else:
+            print(f"El año {year} es bisiesto")
+            return True
+    else:
+        print(f"El año {year} no es bisiesto")
+        return False
+
--- src/maximums.py
+++ src/maximums.py
@@ -1,6 +1,5 @@
-def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
-    biggest = x
+def max_of_two(x :int, y :int) -> int:
+    biggest:int = x
     if x >= y:
         return biggest
     else:
@@ -8,6 +7,5 @@ def max_of_two(x, y):
         return biggest
 
 # Replace the "ANSWER HERE" for your answer
-def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+def max_of_three(x, y, z) -> int:
+    return max_of_two(x,max(y,z))
